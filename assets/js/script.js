bd6b1831c337bea91ea738361bc12c75e7e89fed
mukeshhr22 <msihag028@gmail.com>
2023-06-22 14:39:59 +0530

    final-commit-submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,11 +45,71 @@ $('.move_sl').slick({
                 slidesToShow: 1,
                 slidesToScroll: 1
             }
-        }
+        },
         // You can unslick at a given breakpoint now by adding:
         // settings: "unslick"
         // instead of a settings object
     ]
 });
 
-AOS.init();
+
+$(".slick-slider-custom").slick({
+    slidesToShow: 3,
+    infinite: true,
+    slidesToScroll: 1,
+    centermode: true,
+    cssEase: "linear",
+    arrows: true,
+    prevArrow: $(".prev"),
+    nextArrow: $(".Next"),
+    autoplaySpeed: 2000,
+
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+                infinite: true,
+                slidesToScroll: 1,
+                // autoplay: true,
+            },
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                infinite: true,
+                slidesToScroll: 1,
+                // autoplay: true,
+            },
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                infinite: true,
+                slidesToScroll: 1,
+                // autoplay: true,
+            },
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                infinite: true,
+                slidesToScroll: 1,
+                // autoplay: true,
+            },
+        },
+        {
+            breakpoint: 321,
+            settings: {
+                slidesToShow: 1,
+                infinite: true,
+                slidesToScroll: 1,
+                // autoplay: true,
+            },
+        },
+    ],
+});
+
